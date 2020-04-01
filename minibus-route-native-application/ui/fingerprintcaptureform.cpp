85dc23934990d0930e8eeaf39da994d9829e5a25
ZSingano <zsingano@csir.co.za>
2020-04-01 22:30:53 +0200

    feat(fingerprintcaptureform): Add fingerprint capture functionality

--- minibus-route-native-application/ui/fingerprintcaptureform.cpp
+++ minibus-route-native-application/ui/fingerprintcaptureform.cpp
@@ -6,7 +6,62 @@ FingerprintCaptureForm::FingerprintCaptureForm(QWidget *parent) :
     ui(new Ui::FingerprintCaptureForm)
 {
     ui->setupUi(this);
+
+    /* initialize fields */
+    stored_image_counter_ = 0;
+    c_buffer_size_        = 500;
+    current_image_width_  = c_buffer_size_;
+    current_image_height_ = c_buffer_size_;
+    current_image_        = new (nothrow) unsigned char[current_image_width_*current_image_height_];
+
+    /* setup scanner */
+    scanner_ = new FutronicScanner();
+
+    /* set is streaming control status */
+    is_streaming_ = false;
+    stop_streaming_ = false;
+
+    /* connect statements */
+    connect(scanner_, SIGNAL(link(int)), this, SLOT(on_update_screen_during_streaming(int)));
+    connect(scanner_, SIGNAL(image_link(QByteArray,int)), this, SLOT(on_image_receive(QByteArray,int)));
 }
+
+void FingerprintCaptureForm::on_update_screen_during_streaming(int flag)
+{
+  /* clear display */
+  ui->FingerprintCaptureLabel->clear();
+
+  if(flag == -1)
+  {
+      ui->FingerprintCaptureLabel->setText("Please Place Finger on Scanner");
+      /*ui->quality_progressBar->setValue(0);*/
+  }
+  else
+  {
+      /* get image of stored struct */
+      QImage image(current_image_,\
+                   current_image_width_,\
+                   current_image_height_,\
+                   QImage::Format_Indexed8);
+
+      /* display image of stored struct */
+      ui->FingerprintCaptureLabel->setPixmap(QPixmap::fromImage(image));
+
+      /* display quality of stored struct */
+      /*ui->quality_progressBar->setValue(int(flag));*/
+
+      if(!is_streaming_)
+      {
+        current_image_width_  = c_buffer_size_;
+        current_image_height_ = c_buffer_size_;
+      }
+  }
+  // update interface
+  ui->FingerprintCaptureLabel->repaint();
+  QCoreApplication::processEvents();
+}
+
+
 
 FingerprintCaptureForm::~FingerprintCaptureForm()
 {
@@ -16,4 +71,83 @@ FingerprintCaptureForm::~FingerprintCaptureForm()
 void FingerprintCaptureForm::on_HomePushButton_clicked()
 {
     emit home_button_clicked_signal();
+}
+
+void FingerprintCaptureForm::on_CapturePushButton_clicked()
+{
+    ui->FingerprintCaptureLabel->clear();
+
+    if( !is_streaming_ )
+    {
+        is_streaming_ = true;
+        stop_streaming_ = false;
+        scanner_->setControl(stop_streaming_);
+
+        /* open scanner */
+        scanner_->open(0);
+
+        /* start stream */
+        scanner_->stream(current_image_, current_image_width_, current_image_height_, stop_streaming_ );
+    }
+    else
+    {
+        /* set stop streaming control */
+        stop_streaming_ = true;
+        scanner_->setControl(stop_streaming_);
+
+        /* set is streaming */
+        is_streaming_ = false;
+
+        /* update threads */
+        QCoreApplication::processEvents();
+        /* close scanner */
+        scanner_->close();
+
+        /* display image */
+        ui->FingerprintCapturedLabel->setPixmap(QPixmap::fromImage(captured_image_));
+
+        /* reset variables */
+        current_image_width_  = c_buffer_size_;
+        current_image_height_ = c_buffer_size_;
+    }
+    return;
+}
+
+void FingerprintCaptureForm::on_image_receive(QByteArray Image, int quality)
+{
+    /* check quality against threshold */
+    if(quality >= 50)
+    {
+        /* create new finger Struct */
+        fingerData current_finger;
+        current_finger.initialiseMemory(current_image_width_*current_image_height_);
+
+        /* populate values to struct */
+        current_finger.setFingerDataValues((unsigned char*)Image.data(),\
+                                            quality,
+                                            current_image_width_, \
+                                            current_image_height_);
+
+        /* save image captured */
+        QImage image(current_finger.image,\
+                     current_finger.image_width,\
+                     current_finger.image_height,\
+                     QImage::Format_Indexed8);
+
+        captured_image_ = image;
+    }
+
+    /* test update stream from here */
+    on_update_screen_during_streaming(quality);
+}
+
+void FingerprintCaptureForm::on_FingerprintSavePushButton_clicked()
+{
+    /* save image */
+    QString current_file_name = QString("%1.png")
+                                .arg(QDateTime::currentDateTime().toString("yyMMdd_hh-mm-ss"));
+    captured_image_.save(current_file_name,"png");
+
+    /* clear captured content */
+    ui->FingerprintCapturedLabel->clear();
 }
--- minibus-route-native-application/ui/fingerprintcaptureform.h
+++ minibus-route-native-application/ui/fingerprintcaptureform.h
@@ -1,7 +1,11 @@
 #ifndef FINGERPRINTCAPTUREFORM_H
 #define FINGERPRINTCAPTUREFORM_H
 
+#include <QImage>
+#include <QMainWindow>
 #include <QWidget>
+#include <scanner.h>
+#include <futronic_scanner.h>
 
 namespace Ui {
 class FingerprintCaptureForm;
@@ -15,14 +19,89 @@ public:
     explicit FingerprintCaptureForm(QWidget *parent = nullptr);
     ~FingerprintCaptureForm();
 
+public slots:
+
+    /*!
+    * \brief on_update_screen_during_streaming
+    * \param Flag
+    */
+    void on_update_screen_during_streaming(int flag);
+
+    /*!
+     * \brief on_image_receive
+     * \param Image
+     * \param quality
+     */
+    void on_image_receive(QByteArray Image, int quality);
+
 signals:
     void home_button_clicked_signal();
 
 private slots:
     void on_HomePushButton_clicked();
 
+    void on_CapturePushButton_clicked();
+
+    void on_FingerprintSavePushButton_clicked();
+
 private:
     Ui::FingerprintCaptureForm *ui;
+
+    Scanner* scanner_;
+
+    /*!
+    * \brief The fingerData struct
+    */
+    struct fingerData
+    {
+      unsigned char *image;
+      int quality_score;
+      int image_width;
+      int image_height;
+
+      // assign memory
+      /*!
+       * \brief initialiseMemory
+       * \param size
+       */
+      void initialiseMemory(int size)
+      {
+          this->image = new (nothrow) unsigned char[size];
+      }
+
+      // assign values during streaming
+      /*!
+       * \brief setFingerDataValues
+       * \param image
+       * \param quality_score
+       * \param image_width
+       * \param image_height
+       */
+      void setFingerDataValues( unsigned char *image, int quality_score,\
+                           int image_width, int image_height )
+      {
+
+          // copying
+          for(int i = 0 ; i < image_width * image_height; i++)
+            this->image[i] = image[i];
+
+          // set values
+          this->quality_score = quality_score;
+          this->image_width   = image_width;
+          this->image_height  = image_height;
+      }
+
+
+    };
+
+    QImage captured_image_;
+    bool is_streaming_;
+    bool stop_streaming_;
+    unsigned char* current_image_;
+    int c_buffer_size_ ;
+    int current_image_width_;
+    int current_image_height_;
+    int stored_image_counter_;
 };
 
 #endif // FINGERPRINTCAPTUREFORM_H
