5278cd974a939d01dab7a813efa8b306544adc52
ZSingano <zsingano@csir.co.za>
2020-05-10 18:59:48 +0200

    feat(person.h): Add class to model system personnel type

--- minibus-route-native-application/models/person.cpp
+++ minibus-route-native-application/models/person.cpp
@@ -1,6 +1,27 @@
-#include "modelsperson.h"
+#include "person.h"
 
-Person::Person(QObject *parent) : QObject(parent)
+Person::Person(QObject *parent, Person *person) : QObject(parent)
 {
+    this->setPersonType(person->personType());
+    this->setPersonData(person->personData());
+}
 
+PersonType Person::personType() const
+{
+    return person_type_;
+}
+
+QVector<QSqlRecord> Person::personData() const
+{
+    return person_data_;
+}
+
+void Person::setPersonData(const QVector<QSqlRecord> &personData)
+{
+    person_data_ = personData;
+}
+
+void Person::setPersonType(const PersonType &personType)
+{
+    person_type_ = personType;
 }
--- minibus-route-native-application/models/person.h
+++ minibus-route-native-application/models/person.h
@@ -2,14 +2,29 @@
 #define PERSON_H
 
 #include <QObject>
+#include <QVector>
+#include <QSqlRecord>
+#include "global.h"
 
 class Person : public QObject
 {
     Q_OBJECT
 public:
-    explicit Person(QObject *parent = nullptr);
+    explicit Person(QObject *parent = nullptr, Person *person = nullptr);
+
+    PersonType personType() const;
+
+    QVector<QSqlRecord> personData() const;
+    void setPersonData(const QVector<QSqlRecord> &personData);
 
 signals:
+
+private:
+
+    PersonType person_type_;
+    QVector<QSqlRecord> person_data_;
+
+    void setPersonType(const PersonType &personType);
 
 };
 
